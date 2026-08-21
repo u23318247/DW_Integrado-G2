e88eec8f0e0901283c0f41403fe1f9f485d6bced
Yair-Jans <yafed616@gmail.com>
2026-08-20 21:45:32 -0500

    Cambios de Jair Quispe

---
+++ Laboratorio2/Yair_Quispe/semanados/src/main/java/com/utp/semanados/exception/ProductoNoEncontradoException.java
@@ -0,0 +1,6 @@
+package com.utp.semanados.exception;
+public class ProductoNoEncontradoException extends RuntimeException {
+public ProductoNoEncontradoException(Long id) {
+super("No existe un producto con id: " + id);
+}
+}
---
+++ Laboratorio2/Yair_Quispe/semanados/src/test/java/com/utp/semanados/SemanadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.semanados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
