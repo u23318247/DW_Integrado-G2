ff4362cb32792cf2e87c2b62ffb5466a47ea89fc
SummaryPuppet <adrianm_sv@hotmail.com>
2026-09-03 22:07:45 -0500

    Eliminacion de mala configuracion

--- src/main/java/com/proyectomdweb/proyectomdweb/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.proyectomdweb.proyectomdweb.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String mensaje) {
-        super(mensaje);
-    }
-}
