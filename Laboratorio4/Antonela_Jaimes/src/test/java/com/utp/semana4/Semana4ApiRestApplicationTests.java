54a9bef2bce78a90a86b87ab746f1cf8f3c32fc1
Antonela Jaimes <antonelajaimesdaza@gmail.com>
2026-09-03 20:46:12 -0500

    Laboratorio 4 Antonela Jaimes: Implementacion de API REST y metodos HTTP

---
+++ Laboratorio4/Antonela_Jaimes/src/test/java/com/utp/semana4/Semana4ApiRestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.utp.semana4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana4ApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
