03d938634f1baf1a5412df95eaabbe99220a977c
Joel Andagua <webprojects29@gmail.com>
2026-09-10 20:52:11 -0500

    Actualizacion de Model Usuario

--- ProyectoDWI-LMLL/dwi_lmll/src/main/java/com/g2/dwi_lmll/model/Usuario.java
+++ ProyectoDWI-LMLL/dwi_lmll/src/main/java/com/g2/dwi_lmll/model/Usuario.java
@@ -1,19 +1,19 @@
 package com.g2.dwi_lmll.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "usuarios")
-public class Usuario implements UserDetails {
+public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,46 +37,4 @@ public class Usuario implements UserDetails {
     @Column(name = "fecha_registro", insertable = false, updatable = false)
     private LocalDateTime fechaRegistro;
 
-    //*-- MÉTODOS OBLIGATORIOS DE USERDETAILS (Spring Security) --*//
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        // Si el correo es del admin, se le asigna doble rol
-        if ("admin@email.com".equals(this.email)) {
-            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
-        } 
-        // Se asigna el rol 'ROLE_USER' por defecto a todos los que se logueen.
-        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
-        
-    }
-    
-    @Override
-    public String getUsername() {
-        return this.email; // Se usa el email como username para el Login
-    }
-
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true; // La cuenta no expira
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true; // La cuenta no está bloqueada
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true; // Las credenciales no expiran
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true; // El usuario está activo
-    }
 }
